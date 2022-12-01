1747d7f66f2723dc4ecee990a72a33e84cde1db4
bharat kumar <bharatkumar18002@gmail.com>
2022-12-01 11:43:28 +0530

    hardcode confriguration

--- www.js
+++ www.js
@@ -10,10 +10,11 @@ const http = require("http");
 //DataBase connection Setup//
 const DBREMOTE = process.env.DATABASE;
 const DBLOCAL = process.env.DATABASE_LOCAL;
-const mongourl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.qapkskq.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+// const mongourl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.qapkskq.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+const DATABASE = `mongodb+srv://bharatkumar:774226@cluster0.qapkskq.mongodb.net/publication?retryWrites=true&w=majority`;
 
 mongoose
-  .connect(mongourl)
+  .connect(DATABASE)
   // .connect(DBLOCAL)
   .then(() => {
     console.log("database connection successful");
